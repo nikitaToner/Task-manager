ef459ff372db0d315f7fc8e521629ebd763b1a28
nikitaToner <x1tle@yandex.ru>
2026-06-04 15:06:16 +0700

    Update and rename auth.cs to AuthServes.cs

--- TaskManager/Services/AuthServes.cs
+++ TaskManager/Services/AuthServes.cs
@@ -9,7 +9,7 @@ using System.Data.SQLite;
 using System.Windows;
 namespace TaskManager.Services
 {
-    public class Auth
+    public class AuthServes
     {
         public User Login(string login, string password)
         {
