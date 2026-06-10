55a33b9ede5fdf354a6bd33497f098f45075619f
nikitaToner <x1tle@yandex.ru>
2026-06-10 09:14:46 +0300

    add log

--- TaskManager/Services/AuthServes.cs
+++ TaskManager/Services/AuthServes.cs
@@ -7,12 +7,17 @@ using TaskManager.Models;
 using TaskManager.Data;
 using System.Data.SQLite;
 using System.Windows;
+using Serilog;
+using Serilog.Core;
+using TaskManager.LoggerSystem;
 namespace TaskManager.Services
 {
     public class AuthServes
     {
+        
         public User Login(string login, string password)
         {
+            var log = Log.ForContext<AuthServes>();
             using (var connection = DbContext.CreateConnection())
             using (var command = new SQLiteCommand("SELECT Id, Login, Password, Name, Role FROM Users WHERE Login = @Login AND Password = @Password", connection))
             {
@@ -22,7 +27,7 @@ namespace TaskManager.Services
                 using (var reader = command.ExecuteReader())
                 {
                     if (!reader.Read()) return null;
-
+                    log.Information("Новый юзеррр");
                     return new User
                     {
                         Id = reader.GetInt32(0),
@@ -32,6 +37,7 @@ namespace TaskManager.Services
                         Role = (UserRole)reader.GetInt32(4),
                     };
                 }
+                
             }
         }
     }
